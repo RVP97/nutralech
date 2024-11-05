5c97bb2cbba654e724ec5dcdb4f8d5a70ace0a28
RVP97 <rovapin@gmail.com>
2024-11-04 20:17:28 -0600

    added redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/blog/:path*",
+        destination: "/",
+        permanent: true,
+        statusCode: 302,
+      },
+      {
+        source: "/contact",
+        destination: "/contacto",
+        permanent: true,
+        statusCode: 301,
+      },
+      {
+        source: "/acerca",
+        destination: "/acerca-de-mi",
+        permanent: true,
+        statusCode: 301,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
